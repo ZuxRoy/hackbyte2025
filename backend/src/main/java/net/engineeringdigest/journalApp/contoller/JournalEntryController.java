eb9715d7e37dc21e5a438f4c28cc9ec8c65bdbdb
Debaleena00 <engineerdebaleena29@gmail.com>
2025-04-05 19:56:24 +0530

    backend initial push

---
+++ backend/src/main/java/net/engineeringdigest/journalApp/contoller/JournalEntryController.java
@@ -0,0 +1,25 @@
+package net.engineeringdigest.journalApp.contoller;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private Map<Long, JournalEntry> journalEntries =new HashMap<>();
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+        @PostMapping
+                public boolean createEntry(@RequestBody JournalEntry myEntry){
+                     journalEntries.put(myEntry.getId(), myEntry);
+                     return true;
+    }
+
+}
